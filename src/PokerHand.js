bc15a2c0e46e3fe96688a335b06e296f5ea26125
eddietindame <eddie.tindame@googlemail.com>
2021-03-25 20:32:08 +0000

    Comparison

--- src/PokerHand.js
+++ src/PokerHand.js
@@ -1,39 +1,95 @@
 export class PokerHand {
 
 	constructor(hand) {
-		const cards = hand.split(' ')
+		let cards = hand.split(' ')
 		if (cards.length !== 5) throw Errors.INVALID_HAND
+		cards = cards.map(card => card.toUpperCase())
 
-		const faceChars = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-		const faceCharsLowerCase = faceChars.map(char => char.toLowerCase())
-		const suitChars = ['s', 'h', 'd', 'c']
+		const validFaceChars = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+		const validSuitChars = ['S', 'H', 'D', 'C']
 		let formattingError = false
 		cards.forEach(card => {
 			if (card.length !== 2) formattingError = true
-			if (!faceCharsLowerCase.includes(card[0].toLowerCase())) formattingError = true
-			if (!suitChars.includes(card[1].toLowerCase())) formattingError = true
+			if (!validFaceChars.includes(card[0])) formattingError = true
+			if (!validSuitChars.includes(card[1])) formattingError = true
 		})
 		if (formattingError) throw Errors.INVALID_HAND
 
+		const order = validFaceChars.join('')
+		// Turn the faces into alphabetical chars for easy comparison
+		const faces = cards.map(card => String.fromCharCode([77 - order.indexOf(card[0])])).sort()
+		const suits = cards.map(card => card[1]).sort()
+
 		this.cards = cards
+		this.faces = faces
+		this.suits = suits
+		this.order = order
+		const { rank, value } = this.rankHand()
+		this.rank = rank
+		this.value = value
 
 		this.printHand()
 	}
 
 	printHand() {
 		console.log({
-			cards: this.cards
+			cards: this.cards,
+			faces: this.faces,
+			suits: this.suits,
+			rank: this.rank,
+			value: this.value
 		})
 	}
 
-	compareWith() {
-		return Result.TIE
+	rankHand() {
+		function count(c, a) {
+			c[a] = (c[a] || 0) + 1
+			return c
+		}
+
+		const flush = this.suits[0] === this.suits[4]
+		const first = this.faces[0].charCodeAt(0)
+		const straight = this.faces.every((face, index) => face.charCodeAt(0) - first === index)
+		const counts = this.faces.reduce(count, {})
+		const duplicates = Object.values(counts).reduce(count, {})
+		const value = this.faces.sort((a, b) => {
+			// Counts are in reverse order - bigger is better
+			const countDiff = counts[b] - counts[a]
+		
+			if (countDiff) return countDiff
+			// If counts don't match return
+			return b > a ? -1 : b === a ? 0 : 1
+		}).join('')
+
+		const rank = (
+			(flush && straight && 1) ||
+			(duplicates[4] && 2) ||
+			(duplicates[3] && duplicates[2] && 3) ||
+			(flush && 4) ||
+			(straight && 5) ||
+			(duplicates[3] && 6) ||
+			(duplicates[2] > 1 && 7) ||
+			(duplicates[2] && 8) ||
+			9
+		)
+
+		return { rank, value }
 	}
 
+	compareWith(hand) {
+		const { rank, value } = hand
+		if (this.rank === rank) {
+			if (this.value < value) return Result.WIN
+			else if (this.value > value) return Result.LOSS
+			else return Result.TIE
+		}
+		if (this.rank < rank) return Result.WIN
+		if (this.rank > rank) return Result.LOSS
+	}
 }
 
 export const Errors = {
-	INVALID_HAND: 'Invalid hand'
+	INVALID_HAND: 'Invalid hand.'
 }
 
 export const Result = {
--- src/PokerHand.test.js
+++ src/PokerHand.test.js
@@ -52,5 +52,50 @@ describe('PokerHand', () => {
 
 			expect(hand1.compareWith(hand2)).toBe(Result.TIE)
 		})
+
+		it('wins', () => {
+			const hand1 = new PokerHand('AC 4S 4S 8C AH')
+			const hand2 = new PokerHand('4S 5S 8C AS AD')
+
+			expect(hand1.compareWith(hand2)).toBe(Result.WIN)
+		})
+
+		it('wins with same rank', () => {
+			const hand1 = new PokerHand('AC 4S AS 8C AH')
+			const hand2 = new PokerHand('AS 5S 2C AS AD')
+
+			expect(hand1.rank).toEqual(hand2.rank)
+			expect(hand1.compareWith(hand2)).toBe(Result.WIN)
+		})
+
+		it('loses', () => {
+			const hand1 = new PokerHand('AC 4S 5S 8C 2H')
+			const hand2 = new PokerHand('4S 5S 8C AS AD')
+
+			expect(hand1.compareWith(hand2)).toBe(Result.LOSS)
+		})
+
+		it('loses with same rank', () => {
+			const hand1 = new PokerHand('AS 5S 2C AS AD')
+			const hand2 = new PokerHand('AC 4S AS 8C AH')
+
+			expect(hand1.rank).toEqual(hand2.rank)
+			expect(hand1.compareWith(hand2)).toBe(Result.LOSS)
+		})
+
+		it('case insensitive comparison', () => {
+			const hand1 = new PokerHand('aC 4S 5S 8C ah')
+			const hand2 = new PokerHand('4S 5S 8c As AD')
+
+			const hand3 = new PokerHand('Ac 4s 4S 8C AH')
+			const hand4 = new PokerHand('4S 5S 8C aS ad')
+
+			const hand5 = new PokerHand('ac 4S 5S 8C 2H')
+			const hand6 = new PokerHand('4s 5S 8c AS AD')
+
+			expect(hand1.compareWith(hand2)).toBe(Result.TIE)
+			expect(hand3.compareWith(hand4)).toBe(Result.WIN)
+			expect(hand5.compareWith(hand6)).toBe(Result.LOSS)
+		})
 	})
 })
