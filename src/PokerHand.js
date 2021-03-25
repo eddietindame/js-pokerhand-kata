92cb08c70199bcd6d6679d53424c69b28c0adde4
eddietindame <eddie.tindame@googlemail.com>
2021-03-25 14:39:59 +0000

    Hand format

--- src/PokerHand.js
+++ src/PokerHand.js
@@ -1,15 +1,45 @@
 export class PokerHand {
 
-	compareWith() {
-		return Result.TIE;
+	constructor(hand) {
+		const cards = hand.split(' ')
+		if (cards.length !== 5) throw Errors.INVALID_HAND
+
+		const faceChars = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+		const faceCharsLowerCase = faceChars.map(char => char.toLowerCase())
+		const suitChars = ['s', 'h', 'd', 'c']
+		let formattingError = false
+		cards.forEach(card => {
+			if (card.length !== 2) formattingError = true
+			if (!faceCharsLowerCase.includes(card[0].toLowerCase())) formattingError = true
+			if (!suitChars.includes(card[1].toLowerCase())) formattingError = true
+		})
+		if (formattingError) throw Errors.INVALID_HAND
+
+		this.cards = cards
+
+		this.printHand()
 	}
 
+	printHand() {
+		console.log({
+			cards: this.cards
+		})
+	}
+
+	compareWith() {
+		return Result.TIE
+	}
+
+}
+
+export const Errors = {
+	INVALID_HAND: 'Invalid hand'
 }
 
 export const Result = {
 	WIN: 1,
 	LOSS: 2,
 	TIE: 3
-};
+}
 
-export default PokerHand;
+export default PokerHand
--- src/PokerHand.test.js
+++ src/PokerHand.test.js
@@ -1,18 +1,56 @@
-import PokerHand, { Result } from './PokerHand.js';
+import PokerHand, { Result, Errors } from './PokerHand.js'
 
 describe('PokerHand', () => {
 
+	describe('formatting and hand size', () => {
+
+		it('cards can be any case', () => {
+			let err
+
+			try { new PokerHand('aC 4S 5S 8c ah') }
+			catch (error) { err = error }
+
+			expect(err).toBe(undefined)
+		})
+
+		it('cards must be of correct format', () => {
+			let err1, err2, err3
+
+			try { new PokerHand('AC 4S 5S 8C AHearts') }
+			catch (error) { err1 = error }
+
+			try { new PokerHand('AC 4S5S 8C AH') }
+			catch (error) { err2 = error }
+
+			try { new PokerHand('!D po 45 @£ ZQ') }
+			catch (error) { err3 = error }
+
+			expect(err1).toBe(Errors.INVALID_HAND)
+			expect(err2).toBe(Errors.INVALID_HAND)
+			expect(err3).toBe(Errors.INVALID_HAND)
+		})
+
+		it('hand must have 5 cards', () => {
+			let err1, err2
+
+			try { new PokerHand('AC 4S 5S 8C') }
+			catch (error) { err1 = error }
+
+			try { new PokerHand('AC 4S 5S 8C AH 2D') }
+			catch (error) { err2 = error }
+
+			expect(err1).toBe(Errors.INVALID_HAND)
+			expect(err2).toBe(Errors.INVALID_HAND)
+		})
+	})
+
 	describe('compareWith()', () => {
 
-		it(`ties`, () => {
+		it('ties', () => {
+			const hand1 = new PokerHand('AC 4S 5S 8C AH')
+			const hand2 = new PokerHand('4S 5S 8C AS AD')
 
-			const hand1 = new PokerHand('AC 4S 5S 8C AH');
-			const hand2 = new PokerHand('4S 5S 8C AS AD');
-
-			expect(hand1.compareWith(hand2)).toBe(Result.TIE);
-
-		});
-
-	});
-
-});
+			expect(hand1.compareWith(hand2)).toBe(Result.TIE)
+		})
+	})
+})
